293e1c1bbf6b851f4745d7e45a73aab7f7c0c4db
Stanislav Tyrinov <tyrinow1994@gmail.com>
2017-08-08 22:24:29 +0300

    fixing some bugs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-    var country = $('#RU');
-    country.children().addClass("RU");
-    // country.children().attr('fill','red');
+
+    // $("body").addClass("ie");
+    var country = $('.mapItem');
     country.hover(function () {
         $(this).css('opacity', 0.5);
     },function () {
@@ -9,5 +9,29 @@ $( document ).ready(function() {
     });
     country.click(function () {
         console.log(this.id);
-    })
+    });
+
+    var country_select = $('#select_country > li');
+    country_select.on('click', function(){
+        country_select.removeClass('current');
+        $(this).addClass('current');
+    });
+
+    var days_select =  $('#select_days > li');
+    days_select.on('click', function(){
+        days_select.removeClass('current');
+        $(this).addClass('current');
+    });
+
+    var type_select = $('#select_type > li');
+    type_select.on('click', function(){
+        type_select.removeClass('current');
+        $(this).addClass('current');
+    });
+
+    var description_select = $('#select_description > li');
+    description_select.on('click', function(){
+        description_select.removeClass('current');
+        $(this).addClass('current');
+    });
 });
